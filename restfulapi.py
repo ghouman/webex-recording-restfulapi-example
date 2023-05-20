8766e68a628b6ab540fcbb4174b3653e7633bb5c
human <ghouman@cisco.com>
2023-05-20 11:30:22 +0800

    add loop call when have many recordings

--- restfulapi.py
+++ restfulapi.py
@@ -196,26 +196,31 @@ def recordings():
     max_num = os.getenv("RECORDING_NUM")
     # api document: https://developer.webex.com/docs/api/v1/recordings, v1/admin/recordings api need admin or compliance officer role
     url = baseApiUrl + api_url + "?max=" + max_num + "&from=" + start_date + "&to=" + end_date
-    response = api_call(url)
+    r = []
     recordings = []
-    print("status code : ", response.status_code)
-    # Do a check on the response. If the access_token is invalid then use refresh
-    # tokent to ontain a new set of access token and refresh token.
-    if response.status_code == 401:
-        get_tokens_refresh()
-        response = api_call(url)
 
-    if response.status_code == 403:
-        print("403 forbidden to request")
-        errormsg = "403 forbidden to request:your account not have privilege to request this api"
-        return render_template("granted.html", errormsg=errormsg)
-    if 'errors' in response.json():
-        errormsg = response.json()['errors'][0]["description"]
-        print(errormsg)
-        return render_template("granted.html", errormsg=errormsg)
-    print("response status code : ", response.status_code)
+    response = api_call_recording(url, r)
 
-    r = response.json()['items']
+    if isinstance(response, list):
+        r = response
+    else:
+        print("status code : ", response.status_code)
+        # Do a check on the response. If the access_token is invalid then use refresh
+        # tokent to ontain a new set of access token and refresh token.
+        if response.status_code == 401:
+            get_tokens_refresh()
+            response = api_call(url)
+
+        if response.status_code == 403:
+            print("403 forbidden to request")
+            errormsg = "403 forbidden to request:your account not have privilege to request this api"
+            return render_template("granted.html", errormsg=errormsg)
+        if 'errors' in response.json():
+            errormsg = response.json()['errors'][0]["description"]
+            print(errormsg)
+            return render_template("granted.html", errormsg=errormsg)
+        print("response status code : ", response.status_code)
+
     for i in range(len(r)):
         getRecordingDetailsUrl = "https://webexapis.com/v1/recordings/" + r[i]['id']
         responseDetail = api_call(getRecordingDetailsUrl)
@@ -245,7 +250,34 @@ def api_call(url):
     headers = {'accept': 'application/json', 'Content-Type': 'application/json',
                'Authorization': 'Bearer ' + accessToken}
     response = requests.get(url=url, headers=headers)
+
     return response
+
+def api_call_recording(url, array=None):
+    accessToken = session['oauth_token']
+
+    headers = {'accept': 'application/json', 'Content-Type': 'application/json',
+               'Authorization': 'Bearer ' + accessToken}
+    response = requests.get(url=url, headers=headers)
+    if response.status_code == 200:
+        if not array:
+            array = response.json().get('items', [])
+        else:
+            array += response.json().get('items', [])
+        next_link = None
+        if 'Link' in response.headers:
+            links = response.headers['Link'].split(',')
+            for link in links:
+                if 'rel="next"' in link:
+                    next_link = link[link.index('<')+1:link.index('>')]  # 提取链接URL
+                    break
+
+        if next_link:
+            api_call_recording(next_link, array)
+        return array
+    else:
+        return response
+
 
 
 if __name__ == '__main__':
